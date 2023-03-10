d7032bfd886fda11ef247c1ac0b3eb7591835b31
David Potapkin <david.potapkin@dataart.com>
2023-03-10 11:43:31 +0200

    init, add graceful shutdown

--- dist/index.js
+++ dist/index.js
@@ -2,15 +2,38 @@
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT;
+const port = (_a = process.env.PORT) !== null && _a !== void 0 ? _a : '8080';
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    next();
+});
 app.get('/', (req, res) => {
     res.send('Express and TypeScript Server');
 });
-app.listen(port, () => {
+// Error-handling
+app.use((err, req, res, next) => {
+    const { message, status } = err;
+    return res.status(status !== null && status !== void 0 ? status : 500).json({ error: message });
+});
+const server = app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+// Graceful Shutdown. Signal to kill all process and to cause program termination
+process.on('SIGTERM', () => {
+    console.info('SIGTERM signal received.');
+    console.log('Closing http server...');
+    server.close(() => {
+        console.log('Http server closed.');
+        process.exit(0); // Kill all EventLoop processes. Argument 0 means exit with a "success" code. The "failure" code is 1
+    });
 });
--- index.ts
+++ index.ts
@@ -1,15 +1,43 @@
-import express, { Express, Request, Response } from 'express';
+import express, { type Express, type Request, type Response } from 'express';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT;
+const port = process.env.PORT ?? '8080';
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  next();
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express and TypeScript Server');
 });
 
-app.listen(port, () => {
+// Error-handling
+app.use((err: { message: string, status: string }, req: any, res: any, next: any) => {
+  const { message, status } = err;
+  return res.status(status ?? 500).json({ error: message });
+});
+
+const server = app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
+
+// Graceful Shutdown. Signal to kill all process and to cause program termination
+process.on('SIGTERM', () => {
+  console.info('SIGTERM signal received.');
+
+  console.log('Closing http server...');
+
+  server.close(() => {
+    console.log('Http server closed.');
+    process.exit(0); // Kill all EventLoop processes. Argument 0 means exit with a "success" code. The "failure" code is 1
+  });
 });
