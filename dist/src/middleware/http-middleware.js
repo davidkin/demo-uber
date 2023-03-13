10a6e1ae6d3d79e81da10464cf9ce3edb52aff2f
David Potapkin <david.potapkin@dataart.com>
2023-03-13 19:31:13 +0200

    fix/milstone-1, updates .gitignore file and removes extra folders

--- dist/src/middleware/http-middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const httpLog = (req, res, next) => {
-    res.on('finish', function () {
-        console.log(req.method, decodeURI(req.url), res.statusCode, res.statusMessage);
-    });
-    next();
-};
-exports.default = httpLog;
